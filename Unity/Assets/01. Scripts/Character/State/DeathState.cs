0c23ed8863e4846d74aa9f944646cf0aeb62dd2e
Dablestar <jbynchoi@gmail.com>
2019-03-30 13:32:12 +0900

    20190330정리

---
+++ Unity/Assets/01. Scripts/Character/State/DeathState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : State
+{
+    // Start is called before the first frame update
+    override public void Start()
+    {
+        _character.PlayAnimation("death", () =>
+        {
+            GameObject.Destroy(_character.gameObject);
+        });
+    }
+}
---
+++ Unity/Assets/01. Scripts/Character/WayPointList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+       int wayPointCount =  transform.childCount;
+        List<GameObject> wayPointList = new List<GameObject>();
+        for(int i = 0; i<wayPointCount; i++)
+        {
+            GameObject wayPointObject = transform.GetChild(i).gameObject;
+            wayPointList.Add(wayPointObject);
+        }
+        //WayPoint 스크립트의 리스트 세팅
+        for (int i = 0; i < wayPointCount; i++)
+        {
+            GameObject wayPointObject = transform.GetChild(i).gameObject;
+            WayPoint wayPointScript = wayPointObject.GetComponent<WayPoint>();
+            wayPointScript.setWayPointList(wayPointList);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
